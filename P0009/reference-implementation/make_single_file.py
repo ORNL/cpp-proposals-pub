ce137af1cc6e13707a725c04ad87db4a21d30e71
David Hollman <dshollm@sandia.gov>
2019-01-23 11:00:39 -0800

    one more fix to line numbers

--- P0009/reference-implementation/make_single_file.py
+++ P0009/reference-implementation/make_single_file.py
@@ -4,7 +4,7 @@ from os.path import dirname, abspath, join as path_join
 import re
 
 mydir = dirname(abspath(__file__))
-github_bits = "github.com/ORNL/cpp-proposals-pub/blob/master/P0009/reference-implementation/include/experimental/bits"
+github_bits = "https://github.com/ORNL/cpp-proposals-pub/blob/master/P0009/reference-implementation/include/experimental/bits"
 
 with open(path_join(mydir, "mdspan_single_file.hpp"), "w+") as o:
     with open(path_join(mydir, "include", "experimental", "mdspan")) as mdspan:
@@ -13,7 +13,7 @@ with open(path_join(mydir, "mdspan_single_file.hpp"), "w+") as o:
             if m:
                 with open(path_join(mydir, "include", "experimental", "bits", m.group(1))) as included:
                     for linenum, ln in enumerate(included.readlines()):
-                        print("#line {0} {1}/{2}#{0}".format(linenum+1, github_bits, m.group(1)), file=o)
+                        print("#line {0} \"{1}/{2}#{0}\"".format(linenum+1, github_bits, m.group(1)), file=o)
                         print(ln, file=o, end='')
             else:
                 print(line, file=o, end='')
